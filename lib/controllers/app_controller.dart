03781e257f13e9f98f73fdb04330830c8b827df1
syukri.suwandha <syukri.suwandha@gli.id>
2021-11-16 06:23:45 +0700

    first commit v 0.0.1

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,7 +1,27 @@
 
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:simple_logger/simple_logger.dart';
+
+final logger = SimpleLogger();
 
 class AppController extends GetxController {
 
 
+  @override
+  void onInit() {
+    super.onInit();
+    logger.info('onInit');
+  }
+
+  @override
+  void refresh() {
+    logger.info('refresh');
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    super.onReady();
+    logger.info("onReady");
+  }
 }
